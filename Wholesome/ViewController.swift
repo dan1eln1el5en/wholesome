8641cf23a4bb61241e293ee2cc9b0e42cb24f151
Daniel Løgstrup Nielsen <daniel@aplacebetweenhere.net>
2019-08-21 14:18:53 +0200

    Initial Commit

---
+++ Wholesome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wholesome
+//
+//  Created by Daniel Nielsen on 21/08/2019.
+//  Copyright © 2019 Wholesome Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
